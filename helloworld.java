05f4fbf5638501b0090647709639d9d672bae5cd
Demian Sukhorukov <demian.a.sukhorukov@gmail.com>
2026-08-31 19:33:31 -0400

    Revert "update helloworld.java"
    This reverts commit edbe44af13f8c3a4ef93330ac9afef58e75aa090.

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,5 @@ public class helloworld{
    public static void main(String[] args)
    {
       System.out.println("Hello world!");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop");
    }
 }
